97e102144ac2c08cd68a9fd76a20aaf1439c749b
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-23 13:45:42 +0200

    Leaflet setup and basic map

--- src/pages/WaterwaysMap.js
+++ src/pages/WaterwaysMap.js
@@ -1,5 +1,24 @@
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
 const WaterwaysMap = () => {
-  return <div>Map</div>;
+  return (
+    <div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default WaterwaysMap;
+
+/*
+<Marker position={[51.505, -0.09]}>
+  <Popup>
+    A pretty CSS3 popup. <br /> Easily customizable.
+  </Popup>
+</Marker>
+*/

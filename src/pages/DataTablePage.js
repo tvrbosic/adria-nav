a7980efa1a180841237b764166456b44eddb3598
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-25 14:48:34 +0200

    Data fetch logic moved from DataTable to DataTablePage

--- src/pages/DataTablePage.js
+++ src/pages/DataTablePage.js
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
+import useHttpRequest from '../hooks/useHttpRequest';
 import Header from '../components/Header';
 import DataTable from '../components/DataTable';
 
 const NavObjectsData = () => {
+  const [objectData, setObjectData] = useState([]);
+  const { sendRequest } = useHttpRequest();
+
+  const dataHandler = (data) => {
+    setObjectData(data.features);
+  };
+
+  useEffect(() => {
+    sendRequest({ url: 'https://plovput.li-st.net/getObjekti/' }, dataHandler);
+  }, [sendRequest]);
+
+  const dataLoaded = objectData.length > 0 ? true : false;
+
   return (
     <>
       <Header />
       <Container>
-        <DataTable />
+        {dataLoaded && (
+          <DataTable
+            headers={[
+              'Redni broj',
+              'Naziv objekta',
+              'PS broj',
+              'E broj',
+              'Tip objekta',
+              'Lučka kapetanija',
+            ]}
+            data={objectData}
+          />
+        )}
       </Container>
     </>
   );

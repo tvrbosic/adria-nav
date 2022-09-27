8fbff863d98346a33bd6e3291b8ea7a92ce6c40a
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-27 09:09:40 +0200

    Style marker popup

--- src/pages/Map/MarkerPopup.js
+++ src/pages/Map/MarkerPopup.js
@@ -1,30 +1,32 @@
+import { Stack } from 'react-bootstrap';
+import L from 'leaflet';
 import { Popup } from 'react-leaflet';
 
 const MarkerPopup = ({ item }) => {
   return (
-    <Popup>
-      <div>
-        <div>
-          <span>Naziv objekta:</span>
-          <span>{item.properties.naziv_objekta}</span>
-        </div>
-        <div>
-          <span>PS broj:</span>
-          <span>{item.properties.ps_br}</span>
-        </div>
-        <div>
-          <span>E broj:</span>
-          <span>{item.properties.e_br}</span>
-        </div>
-        <div>
-          <span>Tip objekta:</span>
-          <span>{item.properties.tip_objekta}</span>
-        </div>
-        <div>
-          <span>Lučka kapetanija:</span>
-          <span>{item.properties.lucka_kapetanija}</span>
-        </div>
-      </div>
+    <Popup offset={L.point(0, -25)}>
+      <Stack gap={1} className='fs-6 p-2'>
+        <Stack direction='horizontal' gap={2}>
+          <span className='fw-bold'>Naziv objekta:</span>
+          <span className='ms-auto'>{item.properties.naziv_objekta}</span>
+        </Stack>
+        <Stack direction='horizontal' gap={2}>
+          <span className='fw-bold'>PS broj:</span>
+          <span className='ms-auto'>{item.properties.ps_br}</span>
+        </Stack>
+        <Stack direction='horizontal' gap={2}>
+          <span className='fw-bold'>E broj:</span>
+          <span className='ms-auto'>{item.properties.e_br}</span>
+        </Stack>
+        <Stack direction='horizontal' gap={2}>
+          <span className='fw-bold'>Tip objekta:</span>
+          <span className='ms-auto'>{item.properties.tip_objekta}</span>
+        </Stack>
+        <Stack direction='horizontal' gap={2}>
+          <span className='fw-bold'>Lučka kapetanija:</span>
+          <span className='ms-auto'>{item.properties.lucka_kapetanija}</span>
+        </Stack>
+      </Stack>
     </Popup>
   );
 };
